30ad516b8d7822e9bb5c0c01a8d46169422a6365
Farida Eryani <matahariku@users.github.com>
2019-06-05 22:34:26 +0200

    www/admin/login.php: - ajout mot de passe "blsi" en champ hidden

--- www/admin/login.php
+++ www/admin/login.php
@@ -95,6 +95,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
                 <label>NOM Prenom</label>
                 <input type="text" name="username" class="form-control" >
+                <input type="hidden" name="password" class="form-control" >
                 <span class="help-block"><?php echo $username_err; ?></span>
             </div>    
             <div class="form-group">
