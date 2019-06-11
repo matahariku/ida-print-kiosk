680747b0a288de8f68658019e3c5a35785165f5c
Farida Eryani <matahariku@users.github.com>
2019-06-12 00:57:21 +0200

    www/admin/upload.php:  - test PDF / PS, seuls types de fichiers acceptes

--- www/admin/upload.php
+++ www/admin/upload.php
@@ -2,15 +2,23 @@
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
+$check = false;
+
+// Vérifier si le fichier est un fichier valide 
 if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    
+    $finfo = finfo_open(FILEINFO_MIME_TYPE); // Retourne le type mime à l'extension mimetype
+    $mime_type= finfo_file($finfo, $target_file) . "\n";
+    finfo_close($finfo);
+    
+if ($mime_type == "application/pdf" ) { $check == true }
+if ($mime_type == "application/postscript" ) { $check == true }
+    
     if($check !== false) {
         echo "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        echo "Seuls les fichiers PDF ou PS sont acceptes";
         $uploadOk = 0;
     }
 }
