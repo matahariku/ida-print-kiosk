484d567802c5aae33cc58bc7a1995e93f1924da2
Farida Eryani <matahariku@users.github.com>
2019-06-11 22:51:14 +0200

    i make link with file solde.css

--- www/admin/solde.php
+++ www/admin/solde.php
@@ -1,3 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <link rel="stylesheet" type="text/css" href="solde.css">
+
+
 <?php
 // Initialize the session
 session_start();
@@ -8,15 +23,40 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
- 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<html>
+<head>
+  <title>Feuilles actuel</title>
+</head>
+
+<body>
+<!-- Contenu principal -->
+<br>
+<br>
+<br>
+<br>
+
+<h1><strong>Votre Feuilles actuel</strong></h1>
+<br>
+<?php 
+
+if(isset($_GET['chercher'])){
+	$chercher = $_GET['chercher'];
+	echo "<b>On Trouve un Nom  prenom : ".$chercher."</b>";
+}
+?>
+
 
 <table border="1">
+
+
 	<tr>
-		<th>row_id</th>
-		<th>date</th>
-		<th>C_N</th>
-		<th>Quantite_actuel</th>					
-	<th>
+		<th bgcolor="#000000">row_id</th>
+		<th bgcolor="#000000">date</th>
+		<th bgcolor="#000000">C_N</th>
+		<th bgcolor="#000000">Quantite_actuel</th>					
+	</tr>
 	
 	<?php
 	Include 'inc/db_connect1.php';
@@ -37,20 +77,28 @@ if(isset($_SESSION['username'])){
 
 	$row_id = 1;
 	while($d = mysqli_fetch_array($data)){
+	
+  
+     
 	?>
 	<tr>
-		<td><?php echo $row_idr++; ?></td>
-		<td><?php echo $d['date']; ?></td>
-		<td><?php echo $d['C_N']; ?></td>
-		<td><?php echo $d['Quantite_actuel']; ?></td>
+
+		<td bgcolor="#66CDAA"><?php echo $row_idr++; ?></td>
+		<td bgcolor="#228B22"><?php echo $d['date']; ?></td>
+		<td bgcolor="#1590FF"><?php echo $d['C_N']; ?></td>
+		<td bgcolor="#4B0082"><?php echo $d['Quantite_actuel']; ?></td>
+	
 	</tr>
+
 	<?php } ?>
 </table>
-
+	<br>
+	<br>
     <p align="left">
         <a href="login.php" class="btn btn-danger">Accueil</a>
     </p>
 
 
+</body>
+</html>
 
-	
