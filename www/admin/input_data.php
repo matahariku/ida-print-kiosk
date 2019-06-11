47b3accab8c4ad347d0d516090cb8b71d23cd6a7
Farida Eryani <matahariku@users.github.com>
2019-06-11 21:10:58 +0200

    change name file : upload-chek.php to be upload.php

--- www/admin/input_data.php
+++ www/admin/input_data.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <link rel="stylesheet" type="text/css" href="input_data.css">
+
+
 <?php
 // Initialize the session
 session_start();
@@ -12,7 +26,11 @@ include ('inc/db_connect1.php');
 
 if(isset($_SESSION['username'])){
             $username = $_SESSION['username'];
-}
+        }
+
+if(isset($_SESSION['C_N'])){
+            $C_N = $_SESSION['C_N'];
+        }
 ?>
 
 <!DOCTYPE html>
@@ -21,70 +39,57 @@ if(isset($_SESSION['username'])){
  <title>BLSI - Photocopier</title>
 </head>
 <body>
- <h1>PHOTOCOPIER</h1>
- 
-<form action="upload-check.php" method="post" enctype="multipart/form-data">
+    <br>
+    <br>
+    <br>
+ <h1><strong>PHOTOCOPIER</strong></h1>
+
+<form action="upload.php" method="post" enctype="multipart/form-data">
     Choississez le fichier a imprimer:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Imprimer PDF,PS,TXT" name="submit">
 </form>
 
 
-        
+
 <?php
                 $equary1="SELECT Quantite_actuel from historique, user where historique.id_user = user.id_user and user.nom_prenom = '".$username."' order by date desc LIMIT 1;";
-								//print ("DEBUG $equary1");
+                                                                //print ("DEBUG $equary1");
                 $data = mysqli_query($base, $equary1);
                 $d = mysqli_fetch_array($data);
 ?>
 
- 
- <form action="input_data.php" method="post">
+
+<form action="input_data.php" method="post">
   <table cellpadding="3" cellspacing="0">
    <tr>
-                <tr><td>QUANTITES PHOTOCOPIE ACTUEL</td><td><?php echo $d['Quantite_actuel']; ?> </td></tr>
-   </tr>
+    <br>
+    <br>
+
+                <tr><td bgcolor="#FF4500">QUANTITES PHOTOCOPIE ACTUEL</td><td><h3><p><?php echo $d['Quantite_actuel']; ?> </p></h3></td></tr>
+
    <tr>
-    <td>QUANTITE COPIER</td>
+    <td bgcolor="#FFA500">QUANTITE COPIER</td>
     <td>:</td>
     <td><input type="number" name="Quantite_copier" required></td>
    </tr>
+
    <tr>
-   </tr>
-   <tr>
-    <td>Quel type de votre choix</td>
+    <td bgcolor="#E0FFFF">QUEL TYPE DE VOTRE CHOIX ?</td>
     <td>:</td>
     <td>
       <select name="C_N" required>
-      <option value="">couleur ou noir et blanc? </option>
+     <!-- <option value="C ou N">couleur ou noir et blanc? </option>-->
       <option value="C">Couleur</option>
-      <option value="N">Noir et Blanc</option>
+      <option value="N">Noir_Blanc</option>
      </select>
     </td>
-   </tr>
-   <tr>
-    <td>&nbsp;</td>
-    <td></td>
-    <td><input type="submit" name="garde" value="GARDE-LE"></td>
-   </tr>
-  </table>
-     <p align="left">
-        <a href="login.php" class="btn btn-danger">Accueil</a>
-     </p>
 
- </form>
-
-
-<form name="form1-1" method="post" action="deconnect.php">
-<table>
-<tr>
-<td width="294">
- <input type="submit" name="Submit" value="Se deconnecter">
- </td>
-</tr>
-<table>
+      <p align="left">
+        <stong><a href="login.php" class="btn btn-danger">Accueil</a></strong>
+    </p>
+</table>
 </form>
-
-
-</body>
+    </body>
 </html>
+
