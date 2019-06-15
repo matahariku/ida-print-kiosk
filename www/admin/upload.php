0c6d76aa8255aaecac9b5e488b22093abf6660c4
Farida Eryani <matahariku@users.github.com>
2019-06-15 11:33:47 +0200

    www/admin/upload.php:  - upload fonctionne

--- www/admin/upload.php
+++ www/admin/upload.php
@@ -1,6 +1,37 @@
 <?php
-$target_dir = "uploads/";
+// Initialize the session
+session_start();
+
+// Check if the user is logged in, if not then redirect to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 350px; padding: 20px; }
+    </style>
+    <link rel="stylesheet" type="text/css" href="accueil.css">
+</head>
+
+<?php
+
+//$target_dir = "uploads/";
+$target_dir = sys_get_temp_dir()."/";
+echo "target_dir=$target_dir<br>";
+$tmp_target_file = $_FILES["fileToUpload"]["tmp_name"];
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+echo "rename $tmp_target_file, $target_file";
+rename($tmp_target_file, $target_file);
+
 $uploadOk = 1;
 $check = false;
 
@@ -11,15 +42,22 @@ if(isset($_POST["submit"])) {
     $mime_type= finfo_file($finfo, $target_file) . "\n";
     finfo_close($finfo);
     
-if ($mime_type == "application/pdf" ) { $check == true }
-if ($mime_type == "application/postscript" ) { $check == true }
+if ($mime_type == "application/pdf" ) { $check = true; }
+if ($mime_type == "application/postscript" ) { $check = true; }
+if ($mime_type == "application/text" ) { $check = true; }
+if ($mime_type == "text/plain" ) { $check = true; }
+if ($mime_type == "text/html" ) { $check = true; }
     
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+    if($check == true) {
+        echo "File is supported";
         $uploadOk = 1;
     } else {
-        echo "Seuls les fichiers PDF ou PS sont acceptes";
+        echo "target_file: $target_file";
+        echo "Seuls les fichiers PDF, PS, TXT ou HTML sont acceptes (mime detecte: $mime_type)";
         $uploadOk = 0;
     }
 }
+
+// PDF2PS
+// TXT2PS
 ?>
