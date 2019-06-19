36e43e4dcb8acef09eb2d7a97750aa741f9a0994
Farida Eryani <matahariku@users.github.com>
2019-06-19 15:11:38 +0200

    - re-ajout de bootstrap.css pour accueilcomplete.php seulement, car necessaire

--- www/admin/accueilcomplete.php
+++ www/admin/accueilcomplete.php
@@ -14,6 +14,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
