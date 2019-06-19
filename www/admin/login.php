61efb823254b2724e001b483d0f308a4f0faaff3
Farida Eryani <matahariku@users.github.com>
2019-06-19 14:52:23 +0200

    - suppression de la css bootstrap partout (non utilisee)

--- www/admin/login.php
+++ www/admin/login.php
@@ -81,7 +81,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
--- www/admin/solde.php
+++ www/admin/solde.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style>
         body{ font: 14px sans-serif; }
         .wrapper{ width: 350px; padding: 20px; }
